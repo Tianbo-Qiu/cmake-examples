f4bef3a3435804902081def1a1779074f94066eb
Tianbo <tianboqiu@gmail.com>
2022-08-21 09:34:06 -0700

    headers-include

---
+++ hello-headers/build/CMakeFiles/hello_headers.dir/src/adder.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/hello_headers.dir/src/adder.cpp.o: \
+  /Users/tianboqiu/c/cpp/cmake-examples/hello-headers/src/adder.cpp \
+  /Users/tianboqiu/c/cpp/cmake-examples/hello-headers/include/adder.h
---
+++ hello-headers/src/main.cpp
@@ -0,0 +1,7 @@
+#include "adder.h"
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+  std::cout << add(1, 2) << '\n';
+  return 0;
+}
